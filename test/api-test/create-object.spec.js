aa1cb9f7da684546b600381b4724a1d10af1f590
Fugang Cui <fugang>
2022-05-30 03:32:21 +0800

    addapi test for multiple object when create object

--- test/api-test/create-object.spec.js
+++ test/api-test/create-object.spec.js
@@ -22,6 +22,25 @@ describe('API Test - Create Object', () => {
       .send({})
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(422, done);
+      .expect(422)
+      .end((err, response) => {
+        expect(response.body.error).toEqual('"value" must have at least 1 key');
+        done();
+      });
+  });
+
+  it('should return error when given more than one object', async (done) => {
+    request(process.env.API_BASE_URL)
+      .post('/object')
+      .send({ key1: 'value1', key2: 'value2' })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(422)
+      .end((err, response) => {
+        expect(response.body.error).toEqual(
+          '"value" must have less than or equal to 1 key',
+        );
+        done();
+      });
   });
 });
